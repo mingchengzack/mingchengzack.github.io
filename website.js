cdf755d5bc2a148080857e1b8ba6081d4d70e71e
mingchengzack <chengmingjtt123@gmail.com>
2019-12-10 15:05:52 -0800

    added contents

--- website.js
+++ website.js
@@ -1,22 +1,5 @@
 var main = function() {
-    $("#nav a span").click(function(){
-        var location = $(this).text().charAt(0);
-        var pos = 0;
-		switch(location){
-			case 'A':
-				pos = $("#about").offset().top;
-				break;
-			case 'P':
-				pos = $("#projects").offset().top;
-				break;
-			case 'C':
-				pos = $("#contact").offset().top;
-				break;
-		}
-		
-		pos -= ($(window).height() * 0.1);
-		$(document).scrollTop(pos);
-	});
+
 };
 
 $(document).ready(main);
