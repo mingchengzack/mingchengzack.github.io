cc782bd712647986aec040a7d095cf3f1c8e72fa
mingchengzack <chengmingjtt123@gmail.com>
2020-01-18 22:41:58 -0800

    style change

--- website.js
+++ website.js
@@ -1,5 +1,23 @@
 var main = function() {
-
+  // navigate nav
+  $("#nav a span").click(function() {
+    var location = $(this)
+      .text()
+      .charAt(0);
+    var pos = 0;
+    switch (location) {
+      case "A":
+        pos = $("#about").offset().top;
+        break;
+      case "P":
+        pos = $("#projects").offset().top;
+        break;
+      case "C":
+        pos = $("#contact").offset().top;
+        break;
+    }
+    $(document).scrollTop(pos);
+  });
 };
 
 $(document).ready(main);
